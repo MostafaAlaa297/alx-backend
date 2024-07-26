992b009cb8d2b5be7df20b5da7ac06e3c53e93d5
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-26 06:12:50 -0700

    finish basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0x01. Caching
+"""
+BasicCaching = __import__('0-basic_caching').BasicCaching
+
+
+class BasicCache(BaseCaching):
+    """caching class"""
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        self.cache_data.get(key, None)
