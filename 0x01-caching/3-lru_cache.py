c09850efadfa6040825ca71ff0c6eaf4739857bb
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-26 17:51:05 -0700

    finish lru and mru

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+0x01. Caching
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """caching class"""
+    def __init__(self):
+        """Initialization function"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            print(f"DISCARD: {first_key}")
+            self.cache_data.pop(first_key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
