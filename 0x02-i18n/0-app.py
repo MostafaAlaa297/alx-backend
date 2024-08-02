e141158437a8aa1f83f1384cb6b27d6883fdd7f9
MostafaAlaa297 <elaskary609@gmail.com>
2024-08-02 07:23:45 -0700

    finish 1-index

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,24 +2,9 @@
 """
 0x02. i18n
 """
-from flask import Flask, request, render_template
-from flask_babel import Babel, _
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["BABEL_DEFAULT_LOCALE"] = "en"
-app.config["BABEL_DEFAULT_TIMEZONE"] = "UTC"
-app.config["BABEL_SUPPORTED_LOCALES"] = ["en", "fr"]
-babel = Babel(app)
-
-
-@babellocaleselector
-def get_locale():
-    """Gets the locale from parameter"""
-    lang = request.args.get("lang")
-    if lang in app.config["BABEL_SUPPORTED_LOCALES"]:
-        return lang
-
-    return request.accept_languages.best_match(app.config["BABEL_SUPPORTED_LOCALES"])
 
 
 @app.route("/")
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+0x02. i18n
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+app.config["BABEL_DEFAULT_LOCALE"] = "en"
+app.config["BABEL_DEFAULT_TIMEZONE"] = "UTC"
+app.config["BABEL_SUPPORTED_LOCALES"] = ["en", "fr"]
+babel = Babel(app)
+
+
+@babellocaleselector
+def get_locale():
+    """Gets the locale from parameter"""
+    lang = request.args.get("lang")
+    if lang in app.config["BABEL_SUPPORTED_LOCALES"]:
+        return lang
+
+    return request.accept_languages.best_match(app.config["BABEL_SUPPORTED_LOCALES"])
+
+
+@app.route("/")
+def index():
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
